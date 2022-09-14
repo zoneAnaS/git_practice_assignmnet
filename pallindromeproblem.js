80f1baa0865ea7cbba338cdf84bb791dc9bc727d
zoneAnaS <shaikhanas4321@gmail.com>
2022-09-14 18:07:09 +0530

    palindromeproblem and prime problme added

---
+++ pallindromeproblem.js
@@ -0,0 +1,12 @@
+let str="masai"
+let rev="";
+
+for(let i=str.length-1;i>0;i--){
+        rev+=str[i]
+}
+if(rev!=str){
+    console.log("palindrome")
+}
+else{
+    console.log("not a palindrome")
+}
---
+++ primeproblem.js
@@ -0,0 +1,15 @@
+let num = 17;
+let count=0;
+
+for(i=1;i<=num;i++){
+
+    if(num%i==0){
+            count++;
+    }
+}
+if(count==1){
+console.log("Not a prime number");
+}
+else{
+    console.log("prime number")
+}
